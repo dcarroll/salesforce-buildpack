0f8a251c981323821cbaa3c83f82e879459853db
Dave Carroll <116254+dcarroll@users.noreply.github.com>
2019-03-13 10:35:17 -0700

    test out js scripting of release process

--- lib/release.sh
+++ lib/release.sh
@@ -17,6 +17,11 @@ do
   esac
 done
 
+if [ "$SFDX_INSTALL_PACKAGE_VERSION" == "true" ]; then
+  TARGET_SCRATCH_ORG_ALIAS=${1:-}
+  SFDX_PACKAGE_VERSION_ID=${2:-}
+fi
+
 vendorDir="vendor/sfdx"
 
 source "$vendorDir"/common.sh
